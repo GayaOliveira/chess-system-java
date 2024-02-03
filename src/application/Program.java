24a7571d96ccf018a20ec381bc93762886091f17
GayaOliveira <gmo01233210@gmail.com>
2024-02-03 19:28:40 -0300

    Classe UI criada e chamada no programa principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,14 @@ package application;
 
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
 
 
-        Position pos = new Position(5, 8);
-        System.out.println(pos);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
